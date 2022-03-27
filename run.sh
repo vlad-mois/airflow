1a00c14a0811ef5c2e01b68d52f9d867087c0520
vlad-mois <ijlil@yandex.ru>
2022-03-28 01:33:46 +0300

    Fix run.

--- run.sh
+++ run.sh
@@ -3,46 +3,42 @@
 # Common run script to create appropriate supervisord.conf file.
 
 supervisord_conf="/etc/supervisor/conf.d/supervisord.conf";
-sections="";
+init_script="/etc/supervisor/conf.d/init.sh"
 
-if [[ $RUN_WEB_SERVER == "true" ]]; then
-    echo RUN_WEB_SERVER=$RUN_WEB_SERVER;
-    section=$'\n\n[program:airflow_web_server]\ncommand=/etc/supervisor/conf.d/init.sh && airflow webserver';
-    sections="${sections}${section}";
+function add_section() {
+    local name="${1}"
+    local command="${2}"
+
+    echo "" >> $supervisord_conf;
+    echo "[program:$name]" >> $supervisord_conf;
+    echo "command=$command" >> $supervisord_conf;
+}
+
+if [[ $RUN_WEBSERVER == "true" ]]; then
+    echo RUN_WEBSERVER=$RUN_WEBSERVER;
+    add_section "airflow_webserver" "$init_script && airflow webserver";
 fi
 
 if [[ $RUN_SCHEDULER == "true" ]]; then
     echo RUN_SCHEDULER=$RUN_SCHEDULER;
-    section=$'\n\n[program:airflow_scheduler]\ncommand=/etc/supervisor/conf.d/init.sh && airflow scheduler';
-    sections="${sections}${section}";
+    add_section "airflow_scheduler" "$init_script && airflow scheduler";
 fi
 
 if [[ $RUN_TRIGGERER == "true" ]]; then
     echo RUN_TRIGGERER=$RUN_TRIGGERER;
-    section=$'\n\n[program:airflow_triggerer]\ncommand=/etc/supervisor/conf.d/init.sh && airflow triggerer';
-    sections="${sections}${section}";
+    add_section "airflow_triggerer" "$init_script && airflow triggerer";
 fi
 
 if [[ $RUN_WORKER == "true" ]]; then
     echo RUN_WORKER=$RUN_WORKER;
-    section=$'\n\n[program:airflow_worker]\ncommand=airflow celery worker';
-    sections="${sections}${section}";
+    add_section "airflow_worker" "airflow celery worker";
 fi
 
 if [[ $RUN_FLOWER == "true" ]]; then
     echo RUN_FLOWER=$RUN_FLOWER;
-    section=$'\n\n[program:airflow_flower]\ncommand=airflow celery flower';
-    sections="${sections}${section}";
+    add_section "airflow_flower" "airflow celery flower";
 fi
 
-if [ -z "$sections" ]; then
-    echo "Nothing to run.";
-    exit 0
-fi
-
-echo "Additional supervisord.conf sections: $sections";
-
-echo $sections >> $supervisord_conf;
-
-echo "Starting supervisord...";
+echo "Starting supervisord with conf:";
+printf '%b\n' "$(cat $supervisord_conf)";
 /usr/bin/supervisord
